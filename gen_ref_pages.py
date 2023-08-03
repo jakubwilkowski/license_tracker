db2a8fa9c8cffedd673cf0150a84cc8a44111e2b
Jakub Wilkowski <jakub.wilkowski@plentific.com>
2023-08-03 21:29:28 +0200

    Deployed 7d0c4b7 with MkDocs version: 1.5.2

---
+++ gen_ref_pages.py
@@ -0,0 +1,26 @@
+"""Generate the code reference pages."""
+
+from pathlib import Path
+
+import mkdocs_gen_files
+
+import os
+
+print(os.getcwd())
+
+for path in sorted(Path(f"license_tracker").rglob("*.py")):
+    print(f"found path {path}")
+    module_path = path.relative_to(".").with_suffix("")
+    parts = list(module_path.parts)
+    full_doc_path = Path("license_tracker", "/".join(parts[:])).with_suffix(".md")
+
+    if parts[-1] == "__init__":
+        continue
+    elif parts[-1] == "__main__":
+        continue
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
+
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
